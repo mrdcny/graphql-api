022054228dad52327b625fd54ef27ab7d900e878
Marjay <dacanaymarjay@gmail.com>
2024-12-23 19:38:57 +1000

    fix error on missing env var

--- src/common/utils/config.ts
+++ src/common/utils/config.ts
@@ -6,7 +6,6 @@ dotenv.config();
 export const env = cleanEnv(process.env, {
   NODE_ENV: str({ devDefault: testOnly("test"), choices: ["development", "production", "test"] }),
   PORT: port({ devDefault: testOnly(8080) }),
-  HOST: host({ devDefault: testOnly("localhost") }),
   CORS_ORIGIN: str({ devDefault: testOnly("http://localhost:8080") }),
   DB_URL: str(),
   JWT_SECRET_KEY: str(),
